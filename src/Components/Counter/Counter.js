0ebedce7e906001b92b7e8551c44ffb674f0646f
samar jaoua <jaoua.samar@gmail.com>
2021-12-15 11:35:21 +0100

    setInterval

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
-    state={
-        count:0
+    constructor(props) {
+        super(props)
+    console.log("constructor()")
+        this.state = {
+             count:0,
+             memory:null
+            
+        }
     }
+    
+   componentDidMount(){
+    //    console.log("componentDidMount()")
+    this.setState({ memory: setInterval(() => {
+        this.increment()
+    }, 1000) });
+ 
+   }
+   componentDidUpdate(){
+    //    console.log("componentDidUpdate()")
+  
+   }
     increment = () => {
         this.setState({ count: this.state.count+1 });
         
+    }
+    componentWillUnmount()
+    {
+        // console.log("componentWillUnmount()")
+        clearInterval(this.state.memory)
     }
     decrement=()=>{
         if(this.state.count>0)
         this.setState({count:this.state.count -1})
     }
     render() {
+        console.log("render()")
         return (
             <div>
                <button onClick={this.increment}>+</button>
